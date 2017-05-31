3bf8f3a5f135ead930e30223fe0fdb3132963d9b
Lukas Rusak <lorusak@gmail.com>
2017-05-31 14:34:10 -0700

    linux: allow using linaro msm kernel

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -58,6 +58,11 @@ case "$LINUX" in
     PKG_SOURCE_DIR="xbian-sources-kernel-${PKG_COMMIT}*"   
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
+  dragonboard)
+    PKG_VERSION="180b1b7"
+    PKG_URL="https://git.linaro.org/landing-teams/working/qualcomm/kernel.git/snapshot/$PKG_VERSION.tar.xz"
+    PKG_SOURCE_DIR="$PKG_VERSION*"
+    ;;
   *)
     PKG_VERSION="4.11"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
